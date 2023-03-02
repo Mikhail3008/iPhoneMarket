35ab08f5edb6c21c5589a090df1a0bf94c3c4528
is13119 <is13119@B206-8>
2023-03-02 11:10:40 +0700

    Добавьте файлы проекта.

---
+++ iPhoneMarket/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iPhoneMarket
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null || textBox2 == null || textBox3 == null || textBox4 == null || textBox5 == null || label6 == null) return;
+            else if (textBox1.Text != string.Empty && textBox2.Text != string.Empty && textBox3.Text != string.Empty && textBox4.Text != string.Empty && textBox5.Text != string.Empty && label6.Text != string.Empty)
+            {
+                this.Hide();
+            }
+            iPhone.Add(textBox1);
+
+        }
+    }
+}
---
+++ iPhoneMarket/iPhone.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iPhoneMarket
+{
+    internal class iPhone
+    {
+        public string _model;
+        public int _imei;
+        public string _sost;
+        public int _summ_sk;
+        public int _summ_pr;
+        public int _pribil;
+
+        public string Model
+        {
+            get => _model;
+        }
+
+        public int imei
+        {
+            get => _imei;
+        }
+
+        public string Sost
+        {
+            get => _sost;
+        }
+        public int summ_sk
+        {
+            get => _summ_sk;
+        }
+        public int summ_pr
+        {
+            get => _summ_pr;
+        }
+        public int pribil
+        {
+            get => _pribil;
+        }
+        public iPhone(string Model, int imei, string Sost, int summ_sk, int summ_pr, int pribil)
+        {
+            _model = Model;
+            _imei = imei;
+            _sost = Sost;
+            _summ_sk = summ_sk;
+            _summ_pr = summ_pr;
+            _pribil = pribil;
+        }
+    }
+}
